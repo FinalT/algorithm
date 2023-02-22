2c178841e567cede50d070f3a34e0b0bc3b1c731
FinalT <136178931@qq.com>
2023-02-22 11:54:17 +0800

    leetcode66加一

---
+++ src/day0222/leet66.java
@@ -0,0 +1,18 @@
+package day0222;
+
+public class leet66 {
+
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length-1; i >=0 ; i--) {
+            if (digits[i]!=9){
+                digits[i] += 1;
+                return digits;
+            }else {
+                digits[i]=0;
+            }
+        }
+        digits = new int[digits.length+1];
+        digits[0]=1;
+        return digits;
+    }
+}
