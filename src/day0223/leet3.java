1ac33bb0f5da99d7fd57bdea8b3e3477c0fa5097
FinalT <136178931@qq.com>
2023-02-23 11:13:12 +0800

    leetcode3优化

--- src/day0223/leet3.java
+++ src/day0223/leet3.java
@@ -21,4 +21,21 @@ public class leet3 {
         }
         return max;
     }
+//优化，使用数组
+    public int lengthOfLongestSubstringB(String s) {
+        int[] last = new int[128];
+        for (int i = 0; i < 128; i++) {
+            last[i]=-1;
+        }
+        int res =0;
+        int start = 0;//窗口开始的地方
+        for (int i = 0; i <s.length(); i++) {
+            int index = last[s.charAt(i)];
+            start  = Math.max(start,index+1);
+            res = Math.max(res,index-start+1);
+            last[s.charAt(i)]=i;
+        }
+        return res;
+
+    }
 }
