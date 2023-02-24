f2f0fb241868234bcef778beb19c5441d5068979
FinalT <136178931@qq.com>
2023-02-24 18:59:12 +0800

    leetcode70

---
+++ src/day0224/leet70.java
@@ -0,0 +1,16 @@
+package day0224;
+
+public class leet70 {
+    public int climbStairs(int n) {
+
+        int p = 0;
+        int q = 0;
+        int r = 1;
+        for (int i = 0; i <=n; ++i) {
+            p=q;
+            q=r;
+            r=p+q;
+        }
+        return r;
+    }
+}
