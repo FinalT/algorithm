82063ee15d8bd931e123f950f8e5090f9022a3a3
Finalt <finalt1361@163.com>
2023-02-27 12:39:08 +0800

    leet8

---
+++ src/day0227/leet8.java
@@ -0,0 +1,90 @@
+package day0227;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class leet8 {
+
+    //状态机
+    class Automaton {
+        public int sign = 1;
+        public long ans = 0;
+        private String state = "start";
+        private Map<String, String[]> table = new HashMap<String, String[]>() {{
+            put("start", new String[]{"start", "signed", "in_number", "end"});
+            put("signed", new String[]{"end", "end", "in_number", "end"});
+            put("in_number", new String[]{"end", "end", "in_number", "end"});
+            put("end", new String[]{"end", "end", "end", "end"});
+        }};
+
+        public void get(char c) {
+            state = table.get(state)[get_col(c)];
+            if ("in_number".equals(state)) {
+                ans = ans * 10 + c - '0';
+                ans = sign == 1 ? Math.min(ans, (long) Integer.MAX_VALUE) : Math.min(ans, -(long) Integer.MIN_VALUE);
+            } else if ("signed".equals(state)) {
+                sign = c == '+' ? 1 : -1;
+            }
+        }
+
+        private int get_col(char c) {
+            if (c == ' ') {
+                return 0;
+            }
+            if (c == '+' || c == '-') {
+                return 1;
+            }
+            if (Character.isDigit(c)) {
+                return 2;
+            }
+            return 3;
+        }
+    }
+
+    public int myAtoib(String str) {
+        Automaton automaton = new Automaton();
+        int length = str.length();
+        for (int i = 0; i < length; ++i) {
+            automaton.get(str.charAt(i));
+        }
+        return (int) (automaton.sign * automaton.ans);
+    }
+
+    public int myAtoi(String s) {
+        int sign = 1;
+        int res = 0;
+        int m = s.length();
+        int i = 0;
+        while(i < m && s.charAt(i)==' '){
+            i++;
+        }
+        int start = i;
+        for(; i < m; i++){
+            char c = s.charAt(i);
+            if(i==start && c=='+'){
+                sign = 1;
+            }else if(i==start && c=='-'){
+                sign = -1;
+            }else if(Character.isDigit(c)){
+                int num = c-'0';
+                if(res > Integer.MAX_VALUE/10 || (res == Integer.MAX_VALUE/10&&num>Integer.MAX_VALUE%10)){
+                    return Integer.MAX_VALUE;
+                }
+
+                if(res < Integer.MIN_VALUE/10 || (res == Integer.MIN_VALUE/10&&-num<Integer.MIN_VALUE%10)){
+                    return Integer.MIN_VALUE;
+                }
+                res = res*10+sign*num;
+            }else{
+                break;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        leet8 leet8 = new leet8();
+        int i = leet8.myAtoi("   -7423 to word");
+        System.out.println(i);
+    }
+}
