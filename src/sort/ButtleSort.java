598190d66a34948d5b372862c1413c16503cfacd
Finalt <finalt1361@163.com>
2023-03-01 22:24:46 +0800

    冒泡排序

---
+++ src/sort/ButtleSort.java
@@ -0,0 +1,34 @@
+package sort;
+
+import java.util.Arrays;
+
+public class ButtleSort {
+
+    public int[] sort(int[] arr){
+
+        for (int i = 1; i < arr.length; i++) {
+            boolean flag = true;
+            for (int j = 0; j < arr.length-i; j++) {
+                if (arr[j]>arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=temp;
+
+                    flag=false;
+                }
+            }
+            if (flag){
+                break;
+            }
+        }
+        return arr;
+    }
+
+
+    public static void main(String[] args) {
+        int[] test1 = new int[]{5,4,6,5,1,3};
+        ButtleSort buttleSort = new ButtleSort();
+        buttleSort.sort(test1);
+        System.out.println(Arrays.toString(test1));
+    }
+}
