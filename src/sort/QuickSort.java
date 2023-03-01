9c41b1c1eee287173b0659e91c653bc8372e8047
Finalt <finalt1361@163.com>
2023-03-01 23:15:23 +0800

    快速排序

---
+++ src/sort/QuickSort.java
@@ -0,0 +1,44 @@
+package sort;
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+    public static int[] sort(int[] arr){
+        return quickSort(arr,0,arr.length-1);
+    }
+
+    private static int[] quickSort(int[] arr, int left, int right) {
+        if (left<right){
+            int partition = partition(arr, left, right);
+            quickSort(arr,left,partition-1);
+            quickSort(arr,partition+1,right);
+        }
+        return arr;
+    }
+
+    public static int partition(int[] arr,int left,int right){
+        int pivot = left;
+        int index =pivot+1;
+        for (int i = index; i <= right; i++) {
+            if (arr[i]<arr[pivot]){
+                swap(arr,i,index);
+                index++;
+            }
+        }
+        swap(arr,pivot,index-1);
+        return index-1;
+    }
+
+    public static void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] =arr[j];
+        arr[j] =temp;
+    }
+
+    public static void main(String[] args) {
+        int[] test1 = new int[]{5,4,6,5,1,3};
+        sort(test1);
+        System.out.println(Arrays.toString(test1));
+    }
+}
