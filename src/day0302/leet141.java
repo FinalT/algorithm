756b6e929821fb752e4ef7363bf66667f1136eec
Finalt <finalt1361@163.com>
2023-03-02 11:09:16 +0800

    leet141

---
+++ src/day0302/leet141.java
@@ -0,0 +1,31 @@
+package day0302;
+
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+
+
+public class leet141 {
+
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while (head!=null){
+            if (set.contains(head)){
+                return true;
+            }
+            set.add(head);
+            head=head.next;
+        }
+        return false;
+    }
+}
