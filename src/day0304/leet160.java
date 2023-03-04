00b11d4d2b99ae17638680fa626b398f30e4f63c
FinalT <finalt1361@163.com>
2023-03-04 19:53:22 +0800

    leet160

--- src/day0304/leet160.java
+++ src/day0304/leet160.java
@@ -36,4 +36,15 @@ public class leet160 {
     }
 
 
+    public ListNode getIntersectionNodeB(ListNode headA,ListNode headB){
+        if(headA==null||headB==null)return null;
+        ListNode pA= headA;
+        ListNode pB= headB;
+        while (pA!=pB){
+            pA=pA==null?headB:pA.next;
+            pB=pB==null?headA:pB.next;
+        }
+        return pA;
+    }
+
 }
